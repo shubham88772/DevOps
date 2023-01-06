dee35164d8490d09b814850804f0f875a1a9d1d7
shubhamkumar12 <shubham.kumar12@nagarro.com>
2023-01-06 22:29:19 +0530

    first commit

---
+++ src/test/java/com/nagarro/devops_int1/DevopsInt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.devops_int1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsInt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
